3b38e2ad48b1a6d7e66594beb268c4efb79a5fe9
robroooh <robroo.pc@gmail.com>
2017-10-29 22:18:35 +0100

    make it load kitti dataset

--- main.py
+++ main.py
@@ -148,10 +148,10 @@ def run():
 
     data_dir = './data'
     runs_dir = './runs'
-    tests.test_for_kitti_dataset(data_dir)
 
     # Download pretrained vgg model
     helper.maybe_download_pretrained_vgg(data_dir)
+    tests.test_for_kitti_dataset(data_dir)
 
     # OPTIONAL: Train and Inference on the cityscapes dataset instead of the Kitti dataset.
     # You'll need a GPU with at least 10 teraFLOPS to train on.
