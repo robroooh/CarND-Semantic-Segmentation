2caf026f3b6ff1c05dc28ef42b7cf7e22eadb76d
robroooh <robroo.pc@gmail.com>
2017-10-29 21:06:02 +0100

    first draft, implement everything

--- helper.py
+++ helper.py
@@ -10,6 +10,9 @@ import tensorflow as tf
 from glob import glob
 from urllib.request import urlretrieve
 from tqdm import tqdm
+from imgaug import augmenters as iaa
+from sklearn.model_selection import train_test_split
+
 
 
 class DLProgress(tqdm):
@@ -58,13 +61,177 @@ def maybe_download_pretrained_vgg(data_dir):
         os.remove(os.path.join(vgg_path, vgg_filename))
 
 
-def gen_batch_function(data_folder, image_shape):
+def preprocess_labels(label_image):
+    labels_new = np.zeros_like(label_image)
+    lane_marking_pixels = (label_image[:,:,0] == 6).nonzero()
+    labels_new[lane_marking_pixels] = 1
+    lane_marking_pixels = (label_image[:,:,0] == 7).nonzero()
+    labels_new[lane_marking_pixels] = 1
+    lane_marking_pixels = (label_image[:,:,0] == 10).nonzero()
+    labels_new[lane_marking_pixels] = 2
+
+    vehicle_pixels = (label_image[:,:,0] == 10).nonzero()
+    # Isolate vehicle pixels associated with the hood (y-position > 496)
+    hood_indices = (vehicle_pixels[0] >= 496).nonzero()[0]
+    hood_pixels = (vehicle_pixels[0][hood_indices],
+                   vehicle_pixels[1][hood_indices])
+    # Set hood pixel labels to 0
+    labels_new[hood_pixels] = 0
+    # Identify lane marking pixels (label is 6)
+    return labels_new
+
+def process_carla(BASE_DIR='Train'):
+    image_paths = sorted(glob(os.path.join(BASE_DIR, 'CameraSeg', '*.png')))
+    label_paths = sorted(glob(os.path.join(BASE_DIR, 'CameraRGB', '*.png')))
+    for image, label in zip(image_paths, label_paths):
+        X_train, X_test, y_train, y_test = train_test_split(image_paths,
+                            label_paths, test_size=0.33, random_state=42)
+    if not os.path.exists(os.path.join("training", "CameraRGB")):
+        os.makedirs(os.path.join("training", "CameraRGB"))
+        os.makedirs(os.path.join("training", "CameraSeg"))
+        os.makedirs(os.path.join("valid", "CameraRGB"))
+        os.makedirs(os.path.join("valid", "CameraSeg"))
+    for x,y in zip(X_train,y_train):
+        os.rename(os.path.join(BASE_DIR, "CameraRGB",os.path.basename(x)),
+                 os.path.join("training", "CameraRGB", os.path.basename(x)))
+        os.rename(os.path.join(BASE_DIR,"CameraSeg" ,os.path.basename(y)),
+                 os.path.join("training", "CameraSeg", os.path.basename(y)))
+    for x,y in zip(X_test,y_test):
+        os.rename(os.path.join(BASE_DIR, "CameraRGB",os.path.basename(x)),
+                 os.path.join("valid", "CameraRGB", os.path.basename(x)))
+        os.rename(os.path.join(BASE_DIR,"CameraSeg" ,os.path.basename(y)),
+                 os.path.join("valid", "CameraSeg", os.path.basename(y)))
+
+def split_data(BASE_DIR='data_road'):
+    data_folder = os.path.join(BASE_DIR,'full_training')
+    os.makedirs(data_folder)
+    os.rename(os.path.join(BASE_DIR, 'training'), data_folder)
+    image_paths = glob(os.path.join(data_folder, 'image_2', '*.png'))
+    label_paths = [path for path in glob(os.path.join(data_folder, 'gt_image_2', '*_road_*.png'))]
+    X_train, X_test, y_train, y_test = train_test_split(sorted(image_paths),
+                        sorted(label_paths), test_size=0.33, random_state=42)
+    if not os.path.exists(os.path.join(BASE_DIR,"training", "image_2")):
+        os.makedirs(os.path.join(BASE_DIR,"training", "image_2"))
+        os.makedirs(os.path.join(BASE_DIR,"training", "gt_image_2"))
+        os.makedirs(os.path.join(BASE_DIR,"valid", "image_2"))
+        os.makedirs(os.path.join(BASE_DIR,"valid", "gt_image_2"))
+    for x,y in zip(X_train,y_train):
+        os.rename(os.path.join(BASE_DIR,'full_training', 'image_2',os.path.basename(x)),
+                 os.path.join(BASE_DIR,"training", "image_2", os.path.basename(x)))
+        os.rename(os.path.join(BASE_DIR,'full_training', 'gt_image_2',os.path.basename(y)),
+                 os.path.join(BASE_DIR,"training", "gt_image_2", os.path.basename(y)))
+    for x,y in zip(X_test,y_test):
+        os.rename(os.path.join(BASE_DIR,'full_training', 'image_2',os.path.basename(x)),
+                 os.path.join(BASE_DIR,"valid", "image_2", os.path.basename(x)))
+        os.rename(os.path.join(BASE_DIR,'full_training', 'gt_image_2',os.path.basename(y)),
+             os.path.join(BASE_DIR,"valid", "gt_image_2", os.path.basename(y)))
+
+def gen_batch_carla_function(data_folder, image_shape, train=True):
     """
     Generate function to create batches of training data
     :param data_folder: Path to folder that contains all the datasets
     :param image_shape: Tuple - Shape of image
     :return:
     """
+    _train = train
+    seq = iaa.Sequential([
+        iaa.Fliplr(0.5), # horizontal flips
+        iaa.Crop(percent=(0, 0.1)), # random crops
+        # Small gaussian blur with random sigma between 0 and 0.5.
+        # But we only blur about 50% of all images.
+        # iaa.Sometimes(0.5,
+        #     iaa.GaussianBlur(sigma=(0, 0.5))
+        # ),
+        # Strengthen or weaken the contrast in each image.
+        iaa.ContrastNormalization((0.75, 1.5)),
+        # Add gaussian noise.
+        # For 50% of all images, we sample the noise once per pixel.
+        # For the other 50% of all images, we sample the noise per pixel AND
+        # channel. This can change the color (not only brightness) of the
+        # pixels.
+        # iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5),
+        # Make some images brighter and some darker.
+        # In 20% of all cases, we sample the multiplier once per channel,
+        # which can end up changing the color of the images.
+        # iaa.Multiply((0.8, 1.2), per_channel=0.2),
+        # Apply affine transformations to each image.
+        # Scale/zoom them, translate/move them, rotate them and shear them.
+        iaa.Affine(
+            scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+            translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+            rotate=(-15, 15)
+           # shear=(-8, 8)
+        )
+    ], random_order=True) # apply augmenters in random order
+
+    def get_batches_fn(batch_size):
+        """
+        Create batches of training data
+        :param batch_size: Batch Size
+        :return: Batches of training data
+        """
+        image_paths = glob(os.path.join(data_folder, 'CameraRGB', '*.png'))
+        label_paths = {os.path.basename(path): path for path in glob(os.path.join(data_folder, 'gt_image_2', '*_road_*.png'))}
+        background_color = np.array([255, 0, 0])
+
+        random.shuffle(image_paths)
+        for batch_i in range(0, len(image_paths), batch_size):
+            images = []
+            gt_images = []
+            for image_file in image_paths[batch_i:batch_i+batch_size]:
+                gt_image_file = label_paths[os.path.basename(image_file)]
+                image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
+                gt_image = scipy.misc.imresize(scipy.misc.imread(gt_image_file), image_shape)
+                if _train:
+                    seq_det = seq.to_deterministic()
+                    image = seq_det.augment_image(image)
+                    gt_image = seq_det.augment_image(gt_image)
+                gt_bg = np.all(gt_image == background_color, axis=2)
+                gt_bg = gt_bg.reshape(*gt_bg.shape, 1)
+                gt_image = np.concatenate((gt_bg, np.invert(gt_bg)), axis=2)
+                images.append(image)
+                gt_images.append(gt_image)
+            yield np.array(images), np.array(gt_images)
+    return get_batches_fn
+
+def gen_batch_function(data_folder, image_shape, train=True):
+    """
+    Generate function to create batches of training data
+    :param data_folder: Path to folder that contains all the datasets
+    :param image_shape: Tuple - Shape of image
+    :return:
+    """
+    _train = train
+    seq = iaa.Sequential([
+        iaa.Fliplr(0.5), # horizontal flips
+        iaa.Crop(percent=(0, 0.1)), # random crops
+        # Small gaussian blur with random sigma between 0 and 0.5.
+        # But we only blur about 50% of all images.
+        # iaa.Sometimes(0.5,
+        #     iaa.GaussianBlur(sigma=(0, 0.5))
+        # ),
+        # Strengthen or weaken the contrast in each image.
+        iaa.ContrastNormalization((0.75, 1.5)),
+        # Add gaussian noise.
+        # For 50% of all images, we sample the noise once per pixel.
+        # For the other 50% of all images, we sample the noise per pixel AND
+        # channel. This can change the color (not only brightness) of the
+        # pixels.
+        # iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5),
+        # Make some images brighter and some darker.
+        # In 20% of all cases, we sample the multiplier once per channel,
+        # which can end up changing the color of the images.
+        # iaa.Multiply((0.8, 1.2), per_channel=0.2),
+        # Apply affine transformations to each image.
+        # Scale/zoom them, translate/move them, rotate them and shear them.
+        iaa.Affine(
+            scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+            translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+            rotate=(-15, 15)
+           # shear=(-8, 8)
+        )
+    ], random_order=True) # apply augmenters in random order
+
     def get_batches_fn(batch_size):
         """
         Create batches of training data
@@ -83,20 +250,19 @@ def gen_batch_function(data_folder, image_shape):
             gt_images = []
             for image_file in image_paths[batch_i:batch_i+batch_size]:
                 gt_image_file = label_paths[os.path.basename(image_file)]
-
                 image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
                 gt_image = scipy.misc.imresize(scipy.misc.imread(gt_image_file), image_shape)
-
+                if _train:
+                    seq_det = seq.to_deterministic()
+                    image = seq_det.augment_image(image)
+                    gt_image = seq_det.augment_image(gt_image)
                 gt_bg = np.all(gt_image == background_color, axis=2)
                 gt_bg = gt_bg.reshape(*gt_bg.shape, 1)
                 gt_image = np.concatenate((gt_bg, np.invert(gt_bg)), axis=2)
-
                 images.append(image)
                 gt_images.append(gt_image)
-
             yield np.array(images), np.array(gt_images)
     return get_batches_fn
-
 
 def gen_test_output(sess, logits, keep_prob, image_pl, data_folder, image_shape):
     """
--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import os.path
 import tensorflow as tf
 import helper
 import warnings
+import numpy as np
 from distutils.version import LooseVersion
 import project_tests as tests
 
-
 # Check TensorFlow Version
-assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
+assert LooseVersion(tf.__version__) >= LooseVersion(
+    '1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
 print('TensorFlow Version: {}'.format(tf.__version__))
 
 # Check for a GPU
@@ -16,38 +17,100 @@ if not tf.test.gpu_device_name():
 else:
     print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
 
+LOGDIR = "tensorboard_log/"
 
 def load_vgg(sess, vgg_path):
     """
     Load Pretrained VGG Model into TensorFlow.
-    :param sess: TensorFlow Session
+    :param sess: Tensor Flow Session
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
-    # TODO: Implement function
     #   Use tf.saved_model.loader.load to load the model and weights
+
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
     vgg_keep_prob_tensor_name = 'keep_prob:0'
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
-    return None, None, None, None, None
+
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    with tf.name_scope("Encoder"):
+        image_input = tf.get_default_graph().get_tensor_by_name(vgg_input_tensor_name)
+        # image_input.name = "Image Input"
+        keepprob_out = tf.get_default_graph().get_tensor_by_name(vgg_keep_prob_tensor_name)
+        layer3_out = tf.get_default_graph().get_tensor_by_name(vgg_layer3_out_tensor_name)
+        # layer3_out.name = "Layer 3"
+        layer4_out = tf.get_default_graph().get_tensor_by_name(vgg_layer4_out_tensor_name)
+        # layer4_out.name = "Layer 4"
+        layer7_out = tf.get_default_graph().get_tensor_by_name(vgg_layer7_out_tensor_name)
+        # layer7_out.name = "Layer 7"
+    return image_input, keepprob_out, layer3_out, layer4_out, layer7_out
 tests.test_load_vgg(load_vgg, tf)
 
 
 def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     """
-    Create the layers for a fully convolutional network.  Build skip-layers using the vgg layers.
+    Create the layers for a fully convolutional network.
+    Build skip-layers using the vgg layers.
     :param vgg_layer7_out: TF Tensor for VGG Layer 3 output
     :param vgg_layer4_out: TF Tensor for VGG Layer 4 output
     :param vgg_layer3_out: TF Tensor for VGG Layer 7 output
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
-    # TODO: Implement function
-    return None
+    with tf.name_scope("1x1"):
+        layer7_conv1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding="SAME",
+                                          kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                          kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
+                                          name='layer7_conv1x1')
+        layer4_conv1x1 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, padding="SAME",
+                                          kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                          kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
+                                          name='layer4_conv1x1')
+        layer3_conv1x1 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, padding="SAME",
+                                          kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                          kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
+                                          name="layer3_conv1x1")
+
+    with tf.name_scope("conv2d_transpose"):
+        # 2xConv7
+        layer7_conv1x1_upsamp = tf.layers.conv2d_transpose(layer7_conv1x1,
+                                                           num_classes, 4,
+                                                           strides=2,
+                                                           padding="SAME",
+                                                           kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                                           kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
+                                                           name="layer7_conv1x1_upsamp")
+    with tf.name_scope("Add_Layer"):
+       combine74 = tf.add(layer4_conv1x1, layer7_conv1x1_upsamp, name="combine74")
+
+
+    with tf.name_scope("conv2d_transpose"):
+        # 2xConv4
+        combine74_upsamp = tf.layers.conv2d_transpose(combine74,
+                                                       num_classes, 4,
+                                                       strides=2,
+                                                       padding="SAME",
+                                                       kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                                       kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
+                                                       name="combine74_upsamp")
+
+    with tf.name_scope("Add_Layer"):
+        output = tf.add(layer3_conv1x1, combine74_upsamp, name="combine74_3")
+
+    with tf.name_scope("conv2d_transpose"):
+        # upsample the output to 8x
+        output_upsamp = tf.layers.conv2d_transpose(output,
+                                                   num_classes, 16,
+                                                   strides=8,
+                                                   padding="SAME",
+                                                   kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                                   kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
+                                                   name="output_upsamp")
+
+    return output_upsamp
 tests.test_layers(layers)
 
 
@@ -60,13 +123,24 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-    # TODO: Implement function
-    return None, None, None
-tests.test_optimize(optimize)
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    miou_label = tf.reshape(correct_label[:,:,:,1], [-1])
+    correct_label = tf.reshape(correct_label, (-1, num_classes))
+    with tf.name_scope("xent"):
+        cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
+        tf.summary.scalar("xent", cross_entropy_loss)
+
+    with tf.name_scope("train"):
+        train_op = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy_loss)
+
+    with tf.name_scope("mIOU"):
+        meaniou_op, mean_iou_update_op = tf.metrics.mean_iou(miou_label, tf.nn.softmax(logits)[:,1] > 0.5,num_classes)
+    return logits, train_op, cross_entropy_loss, meaniou_op, mean_iou_update_op
+# tests.test_optimize(optimize)
 
 
 def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
-             correct_label, keep_prob, learning_rate):
+             correct_label, keep_prob, learning_rate, meaniou_op, mean_iou_update_op, get_batches_fn_valid):
     """
     Train neural network and print out the loss during training.
     :param sess: TF Session
@@ -80,17 +154,61 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param keep_prob: TF Placeholder for dropout keep probability
     :param learning_rate: TF Placeholder for learning rate
     """
-    # TODO: Implement function
-    pass
-tests.test_train_nn(train_nn)
+    sess.run(tf.global_variables_initializer())
+    sess.run(tf.local_variables_initializer())
+    writer = tf.summary.FileWriter(LOGDIR+"1")
+    writer.add_graph(sess.graph)
+    train_count = 1
+    # saver = tf.train.Saver()
+    #
+    summ = tf.summary.merge_all()
+    for epoch in range(epochs):
+        for image, label in get_batches_fn(batch_size):
+            # print("=========================")
+            # print(image.shape)
+            # print(label.shape)
+            tf.summary.image('input', image[0], 1)
+            tf.summary.image('ground truth', label[0], 1)
+            with tf.name_scope("loss"):
+
+                _, loss, s, _, miou = sess.run([train_op, cross_entropy_loss, summ, mean_iou_update_op, meaniou_op],
+                                   feed_dict={input_image: image, correct_label: label,
+                                              keep_prob: 0.5, learning_rate: 1e-4})
+            if train_count % 10 == 0:
+                print("Epoch: {} Loss: {} mIOU:{}".format(epoch+1, loss, miou))
+                writer.add_summary(s, train_count)
+            train_count += 1
+        meanLoss = []
+        meanIOU = []
+        for i in range(2):
+            for image, label in get_batches_fn_valid(batch_size):
+                loss, s, _, miou = sess.run([cross_entropy_loss, summ, mean_iou_update_op, meaniou_op],
+                           feed_dict={input_image: image, correct_label: label,
+                                      keep_prob: 1.0, learning_rate: 1e-3})
+                meanLoss.append(loss)
+                meanIOU.append(miou)
+        print("Epoch: {} Validation Loss: {} Validation mIOU:{}".format(epoch+1,
+                                            np.mean(meanLoss), np.mean(miou)))
+
+        # if train_count % 500 == 0:
+        #     saver.save(sess, os.path.join(LOGDIR, "model.ckpt"), i)
+
+# tests.test_train_nn(train_nn)
 
 
 def run():
     num_classes = 2
     image_shape = (160, 576)
+    epochs = 150
+    batch_size = 20
+    correct_label = tf.placeholder(tf.float32, shape=(None, None, None, num_classes))
+    learning_rate = tf.placeholder(tf.float32)
+
     data_dir = './data'
     runs_dir = './runs'
-    tests.test_for_kitti_dataset(data_dir)
+    if not os.path.exists(os.path.join(data_dir, 'data_road/training')):
+        raise FileNotFoundError('Make sure you download dataset and put in data_road')
+    # tests.test_for_kitti_dataset(data_dir)
 
     # Download pretrained vgg model
     helper.maybe_download_pretrained_vgg(data_dir)
@@ -99,23 +217,29 @@ def run():
     # You'll need a GPU with at least 10 teraFLOPS to train on.
     #  https://www.cityscapes-dataset.com/
 
+
     with tf.Session() as sess:
         # Path to vgg model
         vgg_path = os.path.join(data_dir, 'vgg')
         # Create function to get batches
-        get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
+        get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape, train=True)
+        get_batches_fn_valid = helper.gen_batch_function(os.path.join(data_dir, 'data_road/valid'), image_shape, train=False)
 
         # OPTIONAL: Augment Images for better results
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
-
-        # TODO: Build NN using load_vgg, layers, and optimize function
-
-        # TODO: Train NN using the train_nn function
-
-        # TODO: Save inference data using helper.save_inference_samples
-        #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        input_image, keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess, vgg_path)
+        layer_output = layers(layer3_out, layer4_out, layer7_out, num_classes)
+        logits, train_op, cross_entropy_loss, meaniou_op, mean_iou_update_op = optimize(layer_output, correct_label, learning_rate, num_classes)
 
         # OPTIONAL: Apply the trained model to a video
+        print("Running tensorboard in {}".format(LOGDIR+"1"))
+
+        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
+                 correct_label, keep_prob, learning_rate, meaniou_op, mean_iou_update_op, get_batches_fn_valid)
+
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+
+
 
 
 if __name__ == '__main__':
